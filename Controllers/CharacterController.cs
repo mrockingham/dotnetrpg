05909a28fcae17dc8b85943ccabe93d55660c858
Michael Rockingham <mrockingham2@gmail.com.com>
2023-10-13 16:56:20 -0400

    set up model and server

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,39 @@
+
+
+global using dotnet_rpg.Models;
+using dotnet_rpg.Services.CharacterService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    { 
+     
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
+
+        [HttpGet("GetAll")]
+        public ActionResult<Character> Get() {
+
+            return Ok(_characterService.GetAllCharacters());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id) {
+            return Ok(_characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter) 
+        {
+           
+            return Ok(_characterService.AddCharacter(newCharacter));
+        }
+    }
+}
